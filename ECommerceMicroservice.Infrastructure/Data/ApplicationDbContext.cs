49423c109df5382f8106f9758d720fe6a941fd51
levent3 <lvntkrt333@gmail.com>
2025-09-15 16:16:02 +0300

    entiyler ve dbcontextden miras alınıp data katmanı yapıldı

---
+++ ECommerceMicroservice.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using ECommerceMicroservice.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceMicroservice.Infrastructure.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+              // Bire çok ilişki tanımlandı.
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
